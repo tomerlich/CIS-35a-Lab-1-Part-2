5352284bd0e03b0c413fa7497c008d4e4ac1fe40
tomerlich <tomer.erlich2@gmail.com>
2018-09-26 18:25:56 -0700

    commit 2 actually working on the right assignment

---
+++ src/GuessSqrt.java
@@ -0,0 +1,17 @@
+
+public class GuessSqrt {
+	private static long lastGuess;
+	private static long nextGuess;
+	
+	public static double sqrt(long n) {
+		lastGuess =  (long) 1.0;
+		
+		
+		return nextGuess;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
